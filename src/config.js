7a0d2b6b640c4fabce38223b5251ae6e5bab2b8e
brawlcodak <brawlcodak@gmail.com>
2023-04-20 10:49:23 -0500

    revert

--- src/config.js
+++ src/config.js
@@ -11,6 +11,6 @@ module.exports = {
         host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'root',
         password: process.env.MYSQL_PASSWORD || '',
-        database: process.env.MYSQL_DB || 'mayber'
+        database: process.env.MYSQL_DB || 'mayber' 
     }
 }
